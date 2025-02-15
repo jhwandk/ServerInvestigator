990ee226c38a13f1ac08729bbb5012386b02b0d2
jhwandk <106221228+jhwandk@users.noreply.github.com>
2025-02-14 20:59:33 -0800

    Update ServerInvestigator.ps1

--- ServerInvestigator.ps1
+++ ServerInvestigator.ps1
@@ -1,6 +1,6 @@
 # Define the time range
-$StartTime = Get-Date "01/06/2025 07:55:00 AM"
-$EndTime = Get-Date "01/06/2025 08:20:00 AM"
+$StartTime = Get-Date "01/06/2025 08:00:00 AM"
+$EndTime = Get-Date "01/06/2025 08:10:00 AM"
 
 # Define relevant Event IDs
 $RelevantEventIDs = @(6006, 6008, 1074, 41, 1, 4624, 4647)
@@ -10,4 +10,4 @@ Get-WinEvent -LogName System | Where-Object {
     $_.TimeCreated -ge $StartTime -and
     $_.TimeCreated -lt $EndTime -and
     $_.Id -in $RelevantEventIDs
-} | Select-Object TimeCreated, Id, Message | Out-File -FilePath C:\Users\jinhwank\Desktop\RelevantFilteredEvents_Full.txt -Width 300
+} | Select-Object TimeCreated, Id, Message | Out-File -FilePath C:\RelevantFilteredEvents_Full.txt -Width 300
